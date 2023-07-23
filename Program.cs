c3c1e6227ac76335d75b9bb91d7a3f8c28baec59
Pro100Paladin <pro100paladin@icloud.com>
2023-07-23 18:30:03 +0300

    fourth_task

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
-﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
 
+/*
+// вариант 1 [хитрый]
+Console.WriteLine("Введите целое число ");
+    int number = int.Parse(Console.ReadLine()!);
+    int number2 = 2;
+    while (number2 <= number)
+            
+    {
+        Console.Write($"{number2} ");
+        number2+=2;
+    }
 
-Console.WriteLine("введите число");
-int number1 = int.Parse(Console.ReadLine()!);
-int chet = number1%2;
-    if (chet == 0)
-Console.WriteLine("Да");
-    else
-Console.WriteLine("Нет");
+*/
+
+// вариант 2 [правельный]
+
+Console.WriteLine("Введите целое число ");
+int number = int.Parse(Console.ReadLine()!);
+int number2 = 1;
+    while (number2 <= number) 
+    { 
+        if (number2 % 2 == 0)
+        Console.Write($"{number2}, ");
+        number2++;
+    }
+
